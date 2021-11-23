63c8d0a727d571515d34c291619c6f7c4208694a
Caelan Sayler <git@caesay.com>
2021-11-23 12:08:45 +0000

    Remove extra config file, tweak command line options & block loading

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,13 +3,26 @@ using Webber.Server.Blocks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+bool help = false;
 var options = new Mono.Options.OptionSet()
 {
-    {"hw-delete", (_) => { HwInfoBlockServer.Unregister(); Environment.Exit(0); } },
-    {"config=", "Full path to a specific appsettings file to load.", (string path) => { builder.Configuration.AddJsonFile(path, optional: false); } },
+    "",
+    $"Usage { Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName)} [options]",
+    "",
+    "Runs a SignalR server which provides statistics about this machine, local network / environment (even the weather!)",
+    "",
+    "Options:",
+    { "h|?|help", "Shows this help", _ => help = true },
+    {"hw-delete", "Uninstalls the hwinfo driver and exits.", _ => { HwInfoBlockServer.Unregister(); Environment.Exit(0); } },
+    {"c=|config=", "Full path to a specific appsettings file to load.", (string path) => { builder.Configuration.AddJsonFile(path, optional: false); } },
 };
-
 options.Parse(args);
+
+if (help)
+{
+    options.WriteOptionDescriptions(Console.Out);
+    Environment.Exit(0);
+}
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -21,10 +34,16 @@ foreach (var blockServerType in blockServerTypes)
     var config = builder.Configuration.GetSection(blockServerType.Name.Replace("BlockServer", "Block"));
     if (!config.Exists())
         continue;
+
     // Register the block server
     var IBlockServer_TDto = blockServerType.GetInterfaces().Single(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IBlockServer<>));
-    builder.Services.Add(new ServiceDescriptor(IBlockServer_TDto, blockServerType, ServiceLifetime.Singleton));
-    builder.Services.AddSingleton(sp => (IBlockServer) sp.GetRequiredService(IBlockServer_TDto));
+    builder.Services.Add(new ServiceDescriptor(blockServerType, blockServerType, ServiceLifetime.Singleton));
+
+    // Register it's interfaces to use the same instance
+    Func<IServiceProvider, object> factory = (sp) => sp.GetRequiredService(blockServerType);
+    builder.Services.Add(new ServiceDescriptor(IBlockServer_TDto, factory, ServiceLifetime.Singleton));
+    builder.Services.Add(new ServiceDescriptor(typeof(IBlockServer), factory, ServiceLifetime.Singleton));
+
     // Register its configuration
     var configTypeName = blockServerType.FullName.Replace("BlockServer", "BlockConfig");
     var configType = Assembly.GetExecutingAssembly().GetType(configTypeName);
