95c2445d5db50a70b6a100bf4c0d138b7b7ea6be
Caelan Sayler <git@caesay.com>
2021-12-03 11:07:16 +0000

    Fix caelan launch script

--- build-caelan.ps1
+++ build-caelan.ps1
@@ -21,5 +21,5 @@ Set-Location "$PSScriptRoot\webber-react-ui"
 &npm run build
 Copy-Item "build" -Destination "$PSScriptRoot\publish\wwwroot" -Recurse
 
-Set-Content -Path "$PSScriptRoot\publish\run.bat" -Value '%0\..\Webber.exe --urls http://localhost:18902 -c "C:\Source\DesktopUtil\webber-settings.json"'
+Set-Content -Path "$PSScriptRoot\publish\run.bat" -Value '%0\..\Webber.exe --urls http://*:18902 -c "C:\Source\DesktopUtil\webber-settings.json"'
 Write-Host "Done" -ForegroundColor Magenta
