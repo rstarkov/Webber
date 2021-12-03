9f0aa3d95126bccc8d76895167c938618275ee39
Caelan Sayler <git@caesay.com>
2021-12-03 23:38:39 +0000

    Add reload icon

--- webber-react-ui/src/components/App.tsx
+++ webber-react-ui/src/components/App.tsx
@@ -4,6 +4,12 @@ import HwInfoBlock from './HwInfoBlock';
 import ClockBlock from './ClockBlock';
 import WeatherBlock from './WeatherBlock';
 import TimeUntilBlock from './TimeUntilBlock';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+
+function reload() {
+    window.location.reload();
+}
 
 function App() {
     return (
@@ -19,6 +25,9 @@ function App() {
             </div>
             <div className="l1t7 w8h2" style={{ overflow: 'hidden' }}>
                 <TimeUntilBlock />
+            </div>
+            <div onClick={reload} style={{ position: "absolute", right: 0, top: 0, width: 60, height: 60, textAlign: "center", fontSize: 40, opacity: 0.6, lineHeight: "60px" }}>
+                <FontAwesomeIcon icon={faSyncAlt} />
             </div>
         </div>
     );
