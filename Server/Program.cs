db90a48819c20eac9bf51f9593d69945d03be4f5
Caelan Sayler <git@caesay.com>
2021-12-04 21:53:21 +0000

    Add TopShelf support; remove Mono.Options

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,89 +1,20 @@
-﻿using System.Reflection;
+﻿using Topshelf;
 using Webber.Server;
-using Webber.Server.Blocks;
 
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions { Args = args, EnvironmentName = "Development" }); // blazor static files are broken for any other value of EnvironmentName. Use --config to load a custom appsettings.json instead.
+string configPath = null;
 
-bool help = false;
-var options = new Mono.Options.OptionSet()
+Environment.ExitCode = (int) HostFactory.Run(host =>
 {
-    "",
-    $"Usage {Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName)} [options]",
-    "",
-    "Runs a SignalR server which provides statistics about this machine, local network / environment (even the weather!)",
-    "",
-    "Options:",
-    { "h|?|help", "Shows this help", _ => help = true },
-    {"hw-delete", "Uninstalls the hwinfo driver and exits.", _ => { HwInfoBlockServer.Unregister(); Environment.Exit(0); } },
-    {"c=|config=", "Full path to a specific appsettings file to load.", (string path) => { builder.Configuration.AddJsonFile(path, optional: false); } },
-};
-options.Parse(args);
-
-if (help)
-{
-    options.WriteOptionDescriptions(Console.Out);
-    Environment.Exit(0);
-}
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-builder.Services.AddSignalR();
-
-var blockServerTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => !t.IsAbstract && t.IsAssignableTo(typeof(IBlockServer))).ToList();
-foreach (var blockServerType in blockServerTypes)
-{
-    var blockConfig = builder.Configuration.GetSection(blockServerType.Name.Replace("BlockServer", "Block"));
-    if (!blockConfig.Exists())
-        continue;
-
-    // Register the block server
-    var IBlockServer_TDto = blockServerType.GetInterfaces().Single(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IBlockServer<>));
-    builder.Services.Add(new ServiceDescriptor(blockServerType, blockServerType, ServiceLifetime.Singleton));
-
-    // Register it's interfaces to use the same instance
-    Func<IServiceProvider, object> factory = (sp) => sp.GetRequiredService(blockServerType);
-    builder.Services.Add(new ServiceDescriptor(IBlockServer_TDto, factory, ServiceLifetime.Singleton));
-    builder.Services.Add(new ServiceDescriptor(typeof(IBlockServer), factory, ServiceLifetime.Singleton));
-
-    // Register its configuration
-    var configTypeName = blockServerType.FullName.Replace("BlockServer", "BlockConfig");
-    var configType = Assembly.GetExecutingAssembly().GetType(configTypeName);
-    if (configType != null)
-        builder.Services.Add(new ServiceDescriptor(configType, blockConfig.Get(configType)));
-}
-
-var config = builder.Configuration.GetSection("App").GetOrDefault<AppConfig>();
-builder.Services.AddSingleton<AppConfig>(config);
-
-// TODO make this just a single service which toggles "Enabled" on the presence of DbFilePath
-if (String.IsNullOrEmpty(config.DbFilePath))
-    builder.Services.AddSingleton<IDbService, DisabledDbService>();
-else
-    builder.Services.AddSingleton<IDbService>(new DbService(config));
-
-var app = builder.Build();
-
-//app.UseWebAssemblyDebugging();
-app.UseExceptionHandler("/Error");
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-foreach (var service in app.Services.GetServices<IBlockServer>())
-    service.Init(app);
-
-var dbService = app.Services.GetRequiredService<IDbService>();
-dbService.Initialise();
-foreach (var service in app.Services.GetServices<IBlockServer>())
-    service.Start();
-
-app.Run();
-
-class AppConfig
-{
-    public string DbFilePath { get; init; }
-    public string LocalTimezoneName { get; init; }
-}
+    host.AddPersistedCommandLineArgument("config", c => configPath = c);
+    host.Service<WebberService>((d) => new WebberService(configPath));
+    host.SetServiceName("WebberServer");
+    host.SetDisplayName("WebberServer");
+    host.SetDescription("A local signalr service for webber client dashboards");
+    host.EnableShutdown();
+    host.EnableServiceRecovery(r =>
+    {
+        r.RestartService(0); // try to restart immediately
+        r.RestartService(1); // then, try to restart after one minute
+        r.RestartService(10); // then, try to restart the service every 10 minutes after that
+    });
+});
---
+++ Server/TopshelfExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Topshelf;
+using Topshelf.HostConfigurators;
+using Topshelf.Runtime;
+using static Topshelf.Runtime.Windows.NativeMethods;
+
+namespace Webber.Server;
+
+public static class TopshelfExtensions
+{
+    private const int SERVICE_NO_CHANGE = -1;
+
+    [DllImport("Advapi32.dll", EntryPoint = "ChangeServiceConfigW", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+    private static extern bool ChangeServiceConfig(
+        SafeHandle hService,
+        int dwServiceType,
+        int dwStartType,
+        int dwErrorControl,
+        [In] string lpBinaryPathName,
+        [In] string lpLoadOrderGroup,
+        IntPtr lpdwTagId,
+        [In] string lpDependencies,
+        [In] string lpServiceStartName,
+        [In] string lpPassWord,
+        [In] string lpDisplayName
+    );
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct QUERY_SERVICE_CONFIG
+    {
+        [MarshalAs(UnmanagedType.U4)] public uint dwBytesNeeded;
+        [MarshalAs(UnmanagedType.U4)] public uint dwServiceType;
+        [MarshalAs(UnmanagedType.U4)] public uint dwStartType;
+        [MarshalAs(UnmanagedType.U4)] public uint dwErrorControl;
+        [MarshalAs(UnmanagedType.LPWStr)] public string lpBinaryPathName;
+        [MarshalAs(UnmanagedType.LPWStr)] public string lpLoadOrderGroup;
+        [MarshalAs(UnmanagedType.U4)] public uint dwTagID;
+        [MarshalAs(UnmanagedType.LPWStr)] public string lpDependencies;
+        [MarshalAs(UnmanagedType.LPWStr)] public string lpServiceStartName;
+        [MarshalAs(UnmanagedType.LPWStr)] public string lpDisplayName;
+    };
+
+    [DllImport("Advapi32.dll", EntryPoint = "QueryServiceConfigW", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+    private static extern bool QueryServiceConfig(
+        SafeHandle hService,
+        IntPtr lpServiceConfig,
+        uint cbBufSize,
+        out uint pcbBytesNeeded);
+
+    private static List<(string Name, string Value)> _args = new List<(string Name, string Value)>();
+    private static bool _registered = false;
+    private static object _lock = new object();
+
+    public static void AddPersistedCommandLineArgument(this HostConfigurator x, string name, Action<string> callback)
+    {
+        x.AddCommandLineDefinition(name, v =>
+        {
+            lock (_lock)
+            {
+                // only register the install callback once
+                if (!_registered)
+                {
+                    x.AfterInstall(HandleAfterInstall);
+                    _registered = true;
+                }
+
+                // add this arg to be installed
+                _args.Add((name, v));
+
+                // call the original callback
+                callback(v);
+            }
+        });
+    }
+
+    private static void HandleAfterInstall(InstallHostSettings s)
+    {
+        if (!_args.Any())
+            return;
+
+        using var scmHandle = OpenSCManager(null, null, (int) SCM_ACCESS.SC_MANAGER_ALL_ACCESS);
+        if (scmHandle.IsInvalid)
+            throw new Win32Exception();
+
+        using var serviceHandle = OpenService(scmHandle, s.ServiceName, (int) SCM_ACCESS.SC_MANAGER_ALL_ACCESS);
+        if (serviceHandle.IsInvalid)
+            throw new Win32Exception();
+
+        // allocate enough space to hold the service config
+        QueryServiceConfig(serviceHandle, IntPtr.Zero, 0, out var sizeNeeded);
+        if (sizeNeeded == 0)
+            throw new Win32Exception();
+
+        var hGlobal = Marshal.AllocHGlobal((int) sizeNeeded);
+        try
+        {
+            // retrieve existing config
+            if (!QueryServiceConfig(serviceHandle, hGlobal, sizeNeeded, out sizeNeeded))
+                throw new Win32Exception();
+
+            var conf = Marshal.PtrToStructure<QUERY_SERVICE_CONFIG>(hGlobal);
+
+            // add our persisted command line options to the existing image path
+            var args = String.Join(" ", _args.Select(a => $"-{a.Name} \"{a.Value}\""));
+            string newImagePath = conf.lpBinaryPathName + " " + args;
+
+            // persist the new image path
+            if (!ChangeServiceConfig(serviceHandle,
+                SERVICE_NO_CHANGE,
+                SERVICE_NO_CHANGE,
+                SERVICE_NO_CHANGE,
+                newImagePath,
+                null, IntPtr.Zero,
+                null, null, null, null))
+                throw new Win32Exception();
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(hGlobal);
+        }
+    }
+}
