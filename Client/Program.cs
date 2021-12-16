d90252f8e2b03353a89aab683a1ea3e4fce053bd
Roman Starkov <sourcecode@roman.st>
2021-12-16 02:25:45 +0000

    Blazor perf logging

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,4 +10,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<JsInterop>();
 
+PerfTest();
+
 await builder.Build().RunAsync();
+
+void PerfTest()
+{
+    var perftest = new int[256];
+    var rnd = new Random();
+    var start = DateTime.UtcNow;
+    int iters = 0;
+    while (DateTime.UtcNow - start < TimeSpan.FromSeconds(0.1))
+    {
+        for (int i = 0; i < perftest.Length; i++)
+            perftest[i] = rnd.Next();
+        Array.Sort(perftest);
+        iters++;
+    }
+    var time = DateTime.UtcNow - start;
+    Console.WriteLine($"PerfTest: {iters} iterations in {time.TotalMilliseconds:#,0}ms ({iters / time.TotalSeconds:#,0.0} per second)");
+}
