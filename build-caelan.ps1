8c3118f695abf233c78ab5cf5124ffad4a5d6288
Caelan Sayler <git@caesay.com>
2022-01-17 13:02:09 +0000

    Fix startup scripts

--- build-caelan.ps1
+++ build-caelan.ps1
@@ -21,5 +21,7 @@ Set-Location "$PSScriptRoot\webber-react-ui"
 &npm run build
 Copy-Item "build" -Destination "$PSScriptRoot\publish\wwwroot" -Recurse
 
-Set-Content -Path "$PSScriptRoot\publish\run.bat" -Value '%0\..\Webber.exe --urls http://*:18902 -c "C:\Source\DesktopUtil\webber-settings.json"'
+Set-Content -Path "$PSScriptRoot\publish\run.bat" -Value '%0\..\Webber.exe -config "C:\Source\DesktopUtil\webber-settings.json"'
+Set-Content -Path "$PSScriptRoot\publish\install.bat" -Value '%0\..\Webber.exe install -config "C:\Source\DesktopUtil\webber-settings.json" && %0\..\Webber.exe start'
+Set-Content -Path "$PSScriptRoot\publish\uninstall.bat" -Value '%0\..\Webber.exe stop && %0\..\Webber.exe uninstall'
 Write-Host "Done" -ForegroundColor Magenta
