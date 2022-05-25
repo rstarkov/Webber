1b5b3f8d7372fc31ccf5afffef64f5ae0253363f
Roman Starkov <sourcecode@roman.st>
2022-05-25 14:45:13 +0100

    [roman-ui] useWeather and useTimeUntil

---
+++ webber-roman-ui/src/blocks/TimeUntilBlock.tsx
@@ -0,0 +1,30 @@
+import { DateTime } from "luxon";
+import { Config } from "../config";
+import { makeContext } from "../util/makeContext";
+import { BaseDto, useBlock } from "./_BlockBase";
+
+export interface TimeUntilBlockDto extends BaseDto {
+    events: CalendarEvent[];
+}
+
+interface CalendarEvent {
+    displayName: string;
+    startTimeUtc: DateTime;
+    hasStarted: boolean;
+    isNextUp: boolean;
+    isRecurring: boolean;
+}
+
+function dtoPatcher(dto: TimeUntilBlockDto) {
+    for (let i = 0; i < dto.events.length; i++) {
+        dto.events[i].startTimeUtc = DateTime.fromISO(dto.events[i].startTimeUtc as any);
+    }
+}
+
+const ctx = makeContext(() => {
+    const block = useBlock<TimeUntilBlockDto>(`${Config.ServerUrl}/hub/TimeUntilBlock`, dtoPatcher);
+    return block;
+});
+
+export const useTimeUntilBlock = ctx.useFunc;
+export const TimeUntilBlockProvider = ctx.provider;
---
+++ webber-roman-ui/src/blocks/WeatherBlock.tsx
@@ -0,0 +1,31 @@
+import { Config } from "../config";
+import { makeContext } from "../util/makeContext";
+import { BaseDto, useBlock } from "./_BlockBase";
+
+export interface WeatherBlockDto extends BaseDto {
+    curTemperature: number;
+    curTemperatureColor: string;
+    minTemperature: number;
+    minTemperatureColor: string;
+    minTemperatureAtTime: string;
+    minTemperatureAtDay: string;
+    maxTemperature: number;
+    maxTemperatureColor: string;
+    maxTemperatureAtTime: string;
+    maxTemperatureAtDay: string;
+    sunriseTime: string;
+    solarNoonTime: string;
+    sunsetTime: string;
+    sunsetDeltaTime: string;
+}
+
+function dtoPatcher(dto: WeatherBlockDto) {
+}
+
+const ctx = makeContext(() => {
+    const block = useBlock<WeatherBlockDto>(`${Config.ServerUrl}/hub/WeatherBlock`, dtoPatcher);
+    return block;
+});
+
+export const useWeatherBlock = ctx.useFunc;
+export const WeatherBlockProvider = ctx.provider;
