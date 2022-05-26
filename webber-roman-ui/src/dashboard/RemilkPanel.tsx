dd5237a3b68cd52025367dfa322bccfdb6313031
Roman Starkov <sourcecode@roman.st>
2022-05-26 15:40:48 +0100

    [roman-ui] More RTM tweaks

--- webber-roman-ui/src/dashboard/RemilkPanel.tsx
+++ webber-roman-ui/src/dashboard/RemilkPanel.tsx
@@ -9,15 +9,21 @@ function TaskToday(p: { task: RemilkTask }): JSX.Element {
     return <p key={p.task.id}>{overdue && "OVERDUE: "}{p.task.description}</p>
 }
 
-function order(a: RemilkTask, b: RemilkTask): number {
+function byPriority(a: RemilkTask, b: RemilkTask): number {
     if (a.priority != b.priority)
         return a.priority - b.priority;
     if (a.description != b.description)
         return a.description.localeCompare(b.description);
     return a.id.localeCompare(b.id);
 }
+function byDueDate(a: RemilkTask, b: RemilkTask): number {
+    const c = a.dueUtc.toMillis() - b.dueUtc.toMillis();
+    if (c != 0)
+        return c;
+    return a.id.localeCompare(b.id);
+}
 
-const PrioColors = ['', '#f00', '#00f', '#0ff', '#555'];
+const PrioColors = ['', '#EA5200', '#0060BF', '#359AFF', '#555'];
 const TaskDiv = styled.div`
     font-size:90%;
     padding-left: 0.3rem;
@@ -40,32 +46,37 @@ export function RemilkPanel({ ...rest }: React.HTMLAttributes<HTMLDivElement>): 
     const cutoffTomorrow = cutoffStartOfToday.plus({ day: 2 });
     const cutoffSoon = cutoffStartOfToday.plus({ day: 5 });
 
-    const tasksNeglected = tasks.filter(t => t.dueUtc <= cutoffNeglected).sort(order);
-    const tasksOverdue = tasks.filter(t => t.dueUtc > cutoffNeglected && t.dueUtc <= cutoffStartOfToday).sort(order);
-    const tasksToday = tasks.filter(t => t.dueUtc > cutoffStartOfToday && t.dueUtc <= cutoffEndOfToday).sort(order);
-    const tasksTomorrow = tasks.filter(t => t.dueUtc > cutoffEndOfToday && t.dueUtc <= cutoffTomorrow).sort(order);
-    const tasksSoon = tasks.filter(t => t.dueUtc > cutoffTomorrow && t.dueUtc <= cutoffSoon).sort(order);
+    const tasksNeglected = tasks.filter(t => t.dueUtc <= cutoffNeglected).sort(byPriority);
+    const tasksOverdue = tasks.filter(t => t.dueUtc > cutoffNeglected && t.dueUtc <= cutoffStartOfToday).sort(byPriority);
+    const tasksToday = tasks.filter(t => t.dueUtc > cutoffStartOfToday && t.dueUtc <= cutoffEndOfToday).sort(byPriority);
+    const tasksTomorrow = tasks.filter(t => t.dueUtc > cutoffEndOfToday && t.dueUtc <= cutoffTomorrow).sort(byPriority);
+    const tasksSoon = tasks.filter(t => t.dueUtc > cutoffTomorrow && t.dueUtc <= cutoffSoon).sort(byDueDate);
+
+    const showNeglected = tasksNeglected && tasksNeglected.length > 0;
+    const showOverdue = tasksOverdue && tasksOverdue.length > 0;
 
     return <BlockPanelContainer {...rest}>
-        {tasksNeglected && tasksNeglected.length > 0 && <>
+        {showNeglected && <>
             <h3>Neglected</h3>
             {tasksNeglected.map(t => <Task key={t.id} task={t} />)}
         </>}
-        {tasksOverdue && tasksOverdue.length > 0 && <>
+        {showOverdue && <>
             <h3>Overdue</h3>
             {tasksOverdue.map(t => <Task key={t.id} task={t} />)}
         </>}
         {tasksToday && tasksToday.length > 0 && <>
-            <h3>Today</h3>
+            {(showNeglected || showOverdue) && <h3>Today</h3>}
             {tasksToday.map(t => <Task key={t.id} task={t} />)}
         </>}
-        {tasksTomorrow && tasksTomorrow.length > 0 && <>
-            <h3>Tomorrow</h3>
-            {tasksTomorrow.map(t => <Task key={t.id} task={t} />)}
-        </>}
-        {tasksSoon && tasksSoon.length > 0 && <>
-            <h3>Soon</h3>
-            {tasksSoon.map(t => <Task key={t.id} task={t} />)}
-        </>}
+        <div style={{ opacity: 0.3 }}>
+            {tasksTomorrow && tasksTomorrow.length > 0 && <>
+                <h3>Tomorrow</h3>
+                {tasksTomorrow.map(t => <Task key={t.id} task={t} />)}
+            </>}
+            {tasksSoon && tasksSoon.length > 0 && <>
+                <h3>Soon</h3>
+                {tasksSoon.map(t => <Task key={t.id} task={t} />)}
+            </>}
+        </div>
     </BlockPanelContainer >;
 }
