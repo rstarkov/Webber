4bb0351a7c08c7b6f169cd98edc2a21256901f4d
Roman Starkov <sourcecode@roman.st>
2022-11-17 23:46:04 +0000

    [roman-ui] extend night/daytime fill to number line; sharper tick marks; remove vertical grid; cloud tweaks

--- webber-roman-ui/src/pages/WeatherPage.tsx
+++ webber-roman-ui/src/pages/WeatherPage.tsx
@@ -76,7 +76,7 @@ function RainChart(p: { rain: RainCloudPtDto[], cloud: RainCloudPtDto[], hoursSt
     const nightColor = '#013'; // #081133
     const dayColor = '#330';
     const sunlineColor = '#880'; // sunrise & sunset line
-    const gridColor = '#444';
+    const gridColor = '#777';
 
     function getX(dt: DateTime): number { return 100 * (dt.diff(fr)).as('hours') / p.hoursTotal; }
 
@@ -108,7 +108,7 @@ function RainChart(p: { rain: RainCloudPtDto[], cloud: RainCloudPtDto[], hoursSt
         ['#000', '#0000fe', '#0660fe', '#0cbcfe', '#00a300', '#fecb00', '#fe9800', '#fe0000', '#b30000'],
         [0, 0.4, 0.6, 0.75, 0.9, 1, 1, 1, 1]);
     const cloudPts = getPts(p.cloud,
-        ['#aaa0', '#aaa2', '#aaa2', '#aaa2', '#aaa3', '#aaa4', '#aaa5', '#aaa5', '#aaa6', '#aaa7'],
+        ['#aaa0', '#aaa2', '#aaa2', '#aaa2', '#aaa3', '#aaa4', '#aaa5', '#aaa5', '#aaa5', '#aaa5'],
         [0, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 1, 1, 1]);
 
     let firstHour = fr.startOf('hour');
@@ -120,40 +120,46 @@ function RainChart(p: { rain: RainCloudPtDto[], cloud: RainCloudPtDto[], hoursSt
 
     return <svg width='100%' height='100%'>
         <linearGradient id="lighttime" key="lighttime" x1='0' x2='0' y1='0' y2='1'><stop key='1' offset="0%" stopColor='#fff' /><stop key='2' offset="100%" stopColor='#000' /></linearGradient>
-        <mask id='lightmask' key='lightmask'><rect fill='url(#lighttime)' x='0%' y='0%' width='100%' height={chartHeight + '%'} /></mask>
+        <mask id='lightmask' key='lightmask'>
+            <rect key='r1' fill='url(#lighttime)' x='0%' y='0%' width='100%' height={chartHeight + '%'} />
+            <rect key='r2' fill='url(#lighttime)' x='0%' y={chartHeight + '%'} width='100%' height={(115 - chartHeight) + '%'} />
+        </mask>
+        <linearGradient id="cloudgrad" key="cloudgrad" x1='0' x2='0' y1='0' y2='1'><stop key='1' offset="0%" stopColor='#0' /><stop key='2' offset="10%" stopColor='#fff' /></linearGradient>
+        <mask id='cloudmask' key='cloudmask'>
+            <rect fill='url(#cloudgrad)' x='0%' y='0%' width='100%' height={chartHeight + '%'} />
+        </mask>
         <linearGradient id="twilight1gr" key='twilight1gr'><stop key='1' offset="0%" stopColor={nightColor} /><stop key='2' offset="100%" stopColor={dayColor} /></linearGradient>
         <linearGradient id="twilight2gr" key='twilight2gr'><stop key='1' offset="0%" stopColor={dayColor} /><stop key='2' offset="100%" stopColor={nightColor} /></linearGradient>
+
         <g key='glight' mask='url(#lightmask)'>
-            <rect key='night1' x='0%' y='0%' height={chartHeight + '%'} width={getX(sunrise.plus({ hours: -1 })) + '%'} fill={nightColor} />
-            <rect key='twi1' x={getX(sunrise.plus({ hours: -1 })) + '%'} y='0%' height={chartHeight + '%'} width={(100 / p.hoursTotal) + '%'} fill='url(#twilight1gr)' />
-            <rect key='day' x={getX(sunrise) + '%'} y='0%' height={chartHeight + '%'} width={(getX(sunset) - getX(sunrise)) + '%'} fill={dayColor} />
-            <rect key='twi2' x={getX(sunset) + '%'} y='0%' height={chartHeight + '%'} width={(100 / p.hoursTotal) + '%'} fill='url(#twilight2gr)' />
-            <rect key='night2' x={getX(sunset.plus({ hours: 1 })) + '%'} y='0%' height={chartHeight + '%'} width={(100 - getX(sunset.plus({ hours: 1 }))) + '%'} fill={nightColor} />
-        </g>
-
-        <line key='xaxis' x1='0%' x2='100%' y1={chartHeight + '%'} y2={chartHeight + '%'} stroke={gridColor} />
-        {hours.map((hr, i) => <line key={`${i}_hr`} x1={hr.centerX + '%'} x2={hr.centerX + '%'} y1='0%' y2={(chartHeight + tickHeight * 0.7) + '%'} stroke={gridColor} />)}
-
-        <g key='grise' mask='url(#lightmask)'>
-            <line key='sunrise' x1={getX(sunrise) + '%'} x2={getX(sunrise) + '%'} y1='0%' y2={chartHeight + '%'} stroke={sunlineColor} strokeDasharray='3' />
-            <line key='sunset' x1={getX(sunset) + '%'} x2={getX(sunset) + '%'} y1='0%' y2={chartHeight + '%'} stroke={sunlineColor} strokeDasharray='3' />
+            <rect key='night1' x='0%' y='0%' height='100%' width={getX(sunrise.plus({ hours: -1 })) + '%'} fill={nightColor} />
+            <rect key='twi1' x={getX(sunrise.plus({ hours: -1 })) + '%'} y='0%' height='100%' width={(100 / p.hoursTotal) + '%'} fill='url(#twilight1gr)' />
+            <rect key='day' x={getX(sunrise) + '%'} y='0%' height='100%' width={(getX(sunset) - getX(sunrise)) + '%'} fill={dayColor} />
+            <rect key='twi2' x={getX(sunset) + '%'} y='0%' height='100%' width={(100 / p.hoursTotal) + '%'} fill='url(#twilight2gr)' />
+            <rect key='night2' x={getX(sunset.plus({ hours: 1 })) + '%'} y='0%' height='100%' width={(100 - getX(sunset.plus({ hours: 1 }))) + '%'} fill={nightColor} />
         </g>
 
         {hours.filter(hr => (hr.hour % 2) == 0).map((hr, i) => <svg key={`${i}_tx`} x={(hr.centerX - textHeight / 2) + '%'} y={(100 - textHeight) + '%'} width={textHeight + '%'} height={textHeight + '%'} viewBox='0 0 1 1'>
             <text x='0.5' y='0' fontSize='1' fill='#ccc' textAnchor='middle' dominantBaseline='hanging'>{hr.hour.toLocaleString('en-US', { minimumIntegerDigits: 2 })}</text>
         </svg>)}
-        {cloudPts.map((pt, i) => pt.samples.map((sm, j) => {
-            const gap = pt.widthL! + pt.widthR! > 2 ? 0 : pt.widthL! + pt.widthR! > 0.4 ? 0.05 : -0.15 /* negative to make them blend together */;
-            return <rect
-                key={`${i}_${j}_1`}
-                x={`${pt.centerX - pt.widthL! + gap}%`}
-                y={`${chartHeight / 100 * (100 - sm.y - sm.height)}%`}
-                width={`${pt.widthL! + pt.widthR! - gap}%`}
-                height={`${chartHeight / 100 * sm.height}%`}
-                fill={sm.color}
-                strokeWidth='0'>
-            </rect>;
-        }))}
+        <g key='clouds' mask='url(#cloudmask)'>
+            {cloudPts.map((pt, i) => pt.samples.map((sm, j) => {
+                const gap = pt.widthL! + pt.widthR! > 2 ? 0 : pt.widthL! + pt.widthR! > 0.4 ? 0.05 : -0.15 /* negative to make them blend together */;
+                return <rect
+                    key={`${i}_${j}_1`}
+                    x={`${pt.centerX - pt.widthL! + gap}%`}
+                    y={`${chartHeight / 100 * (100 - sm.y - sm.height)}%`}
+                    width={`${pt.widthL! + pt.widthR! - gap}%`}
+                    height={`${chartHeight / 100 * sm.height}%`}
+                    fill={sm.color}
+                    strokeWidth='0'>
+                </rect>;
+            }))}
+        </g>
+        <g key='grise'>
+            <line key='sunrise' x1={getX(sunrise) + '%'} x2={getX(sunrise) + '%'} y1='0%' y2={chartHeight + '%'} stroke={sunlineColor} strokeDasharray='3' />
+            <line key='sunset' x1={getX(sunset) + '%'} x2={getX(sunset) + '%'} y1='0%' y2={chartHeight + '%'} stroke={sunlineColor} strokeDasharray='3' />
+        </g>
         {rainPts.map((pt, i) => pt.samples.map((sm, j) => {
             const gap = pt.widthL! + pt.widthR! > 0.4 ? 0.05 : -0.15 /* negative to make them blend together */;
             return <rect
@@ -166,6 +172,10 @@ function RainChart(p: { rain: RainCloudPtDto[], cloud: RainCloudPtDto[], hoursSt
                 strokeWidth='0'>
             </rect>;
         }))}
+
+        <line key='xaxis' x1='0%' x2='100%' y1={chartHeight + '%'} y2={chartHeight + '%'} stroke={gridColor} />
+        {hours.map((hr, i) => <line key={`${i}_hr`} x1={hr.centerX + '%'} x2={hr.centerX + '%'} y1={chartHeight + '%'} y2={(chartHeight + tickHeight * 0.7) + '%'} stroke={gridColor} strokeWidth={(hr.hour % 2) == 0 ? 3 : 1} />)}
+
         <svg key='tick' x={(getX(DateTime.now()) - tickHeight * 1.3 / 2) + '%'} y={chartHeight - tickHeight * 1.3 * 0.45 + '%'} width={tickHeight * 1.3 + '%'} height={tickHeight * 1.3 + '%'} viewBox='-0.1 -0.2 1.2 1.2'>
             <path d='M 0 1 .5 0 1 1 z' fill='red' stroke='#000' strokeWidth='0.15' strokeLinejoin='miter' />
         </svg>
