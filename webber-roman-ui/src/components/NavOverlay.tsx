764636cc84951cb139cfa55e1c7b3ba9a9ee4ac8
Roman Starkov <sourcecode@roman.st>
2022-11-17 18:48:41 +0000

    [roman-ui] NavOverlay fix for mobile URL bar. Icons.

--- webber-roman-ui/src/components/NavOverlay.tsx
+++ webber-roman-ui/src/components/NavOverlay.tsx
@@ -1,6 +1,8 @@
 import { useDialogState, Dialog, DialogState } from 'ariakit/Dialog';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faArrowsRotate, faExpand, faCompress } from '@fortawesome/free-solid-svg-icons';
 
 const NavOverlayDiv = styled.div`
     position: absolute;
@@ -8,12 +10,17 @@ const NavOverlayDiv = styled.div`
     right: 0;
     top: 0;
     bottom: 0;
+    padding: 10vh 0;
     background: rgba(37, 39, 49, 0.87);
     display: grid;
     align-items: center;
     justify-items: center;
     grid-template-rows: 1fr 1fr 1fr 1fr;
     grid-auto-flow: column;
+`;
+
+const Fi = styled(FontAwesomeIcon)`
+    margin-right: 2vw;
 `;
 
 export function NavOverlay(props: { state: DialogState }): JSX.Element {
@@ -31,15 +38,15 @@ export function NavOverlay(props: { state: DialogState }): JSX.Element {
 
     return <Dialog state={props.state}>
         <NavOverlayDiv>
-            <button onClick={() => navigate('/')}>Page: Main</button>
+            <button onClick={() => navigate('/')}>Page: Tasks</button>
+            <button onClick={() => navigate('/weather')}>Page: Weather</button>
             <button onClick={() => navigate('/classic')}>Page: Classic</button>
             <button onClick={() => navigate('/experiments')}>Page: Experiments</button>
-            <div></div>
 
-            <button onClick={props.state.hide}>Dismiss</button>
+            <button onClick={props.state.hide}><FontAwesomeIcon icon={faXmark} /></button>
             <div></div>
-            <button onClick={() => location.reload()}>Force Reload</button>
-            <button onClick={toggleFullScreen}>{isFullScreen ? 'Exit Full Screen' : 'Full Screen'}</button>
+            <button onClick={() => location.reload()}><Fi icon={faArrowsRotate} />Reload</button>
+            <button onClick={toggleFullScreen}>{isFullScreen ? <><Fi icon={faCompress} />Exit Full Screen</> : <><Fi icon={faExpand} />Full Screen</>}</button>
         </NavOverlayDiv>
     </Dialog>;
 }
