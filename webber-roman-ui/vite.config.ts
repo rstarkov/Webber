888bf80cdbd3664304b8b84b2994e44d20f0e716
Roman Starkov <sourcecode@roman.st>
2022-11-20 20:42:58 +0000

    [roman-ui] lint part 3: also lint config files

--- webber-roman-ui/vite.config.ts
+++ webber-roman-ui/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import { fileURLToPath } from "url"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        app: fileURLToPath(new URL('./index.html', import.meta.url)),
-        wrapper: fileURLToPath(new URL('./wrapper.html', import.meta.url)),
+        app: fileURLToPath(new URL("./index.html", import.meta.url)),
+        wrapper: fileURLToPath(new URL("./wrapper.html", import.meta.url)),
       },
     },
   },
