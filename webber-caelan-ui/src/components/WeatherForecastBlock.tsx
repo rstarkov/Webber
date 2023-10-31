2294e236b0c79f5738307686f1c8fd25c36640a7
Caelan Sayler <git@caesay.com>
2023-10-31 21:42:52 +0000

    [caelan-ui] add end of day and current time to forecast

--- webber-caelan-ui/src/components/WeatherForecastBlock.tsx
+++ webber-caelan-ui/src/components/WeatherForecastBlock.tsx
@@ -37,6 +37,24 @@ const PercipText = styled.div`
     font-size: 24px;
 `;
 
+const NewDay = styled.div`
+    position: absolute;
+    top: -6px;
+    bottom: 6px;
+    width: 4px;
+    border-radius: 3px;
+    background-color: #00C6F5;
+`
+
+const NowTime = styled.div`
+    position: absolute;
+    top: -6px;
+    bottom: 6px;
+    width: 4px;
+    border-radius: 3px;
+    background-color: red;
+`
+
 const WeatherForecastBlock: React.FunctionComponent<{ data: WeatherForecastBlockDto }> = ({ data }) => {
     var t4hours = _.take(data.hours, 24);
 
@@ -45,9 +63,20 @@ const WeatherForecastBlock: React.FunctionComponent<{ data: WeatherForecastBlock
         return prob.toString() + "%";
     }
 
+    const newDay = (24 - moment(t4hours[0].dateTime).hour());
+
+    const start = moment(t4hours[0].dateTime).valueOf();
+    let now = moment().valueOf() - start;
+    let end = moment(t4hours[23].dateTime).add(1, "hour").valueOf() - start;
+
+    const maxWidth = 720;
+    const nowPosition = (now / end) * maxWidth;
+
     return (
         <React.Fragment>
             {_.map(t4hours, (e, i) => (<RainBar style={{ left: i * 30, height: 60 * (e.rainProbability / 100) }} />))}
+            <NewDay style={{ left: newDay * 45 - 2 }} />
+            <NowTime style={{ left: nowPosition - 2 }} />
             <TimeText style={{ left: 90 * 0 }}>{moment(t4hours[1].dateTime).format("HH:mm")}</TimeText>
             <TimeText style={{ left: 90 * 1 }}>{moment(t4hours[4].dateTime).format("HH:mm")}</TimeText>
             <TimeText style={{ left: 90 * 2 }}>{moment(t4hours[7].dateTime).format("HH:mm")}</TimeText>
